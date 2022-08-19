0fd6a05745102e9cfaf1203f734520196e0781b1
Munsterlander <munsterlander@users.noreply.github.com>
2022-08-19 15:55:46 -0700

    Our version in game

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,48 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame_mini_sprite/flame_mini_sprite.dart';
 import 'package:flutter/material.dart';
+import 'package:mini_sprite/mini_sprite.dart';
+import '../assets.dart';
 
 void main() {
-  runApp(GameWidget(game: MyGame()));
+  runApp(GameWidget(game: InvertMe()));
 }
 
-class MyGame extends FlameGame {}
+class InvertMe extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    final miniLibrary = MiniLibrary.fromDataString(library);
+    final sprites = await miniLibrary.toSprites(
+      color: Colors.white,
+      pixelSize: 1,
+    );
+
+    final miniMap = MiniMap.fromDataString(map);
+    for (final entry in miniMap.objects.entries) {
+      final sprite = sprites[entry.value['sprite']];
+      if (sprite != null) {
+        unawaited(
+          add(
+            SpriteComponent(
+              sprite: sprite,
+              position: Vector2(
+                (entry.key.x * 8).toDouble(),
+                (entry.key.y * 8).toDouble(),
+              ),
+              size: Vector2(
+                sprite.image.width.toDouble(),
+                sprite.image.height.toDouble(),
+              ),
+            ),
+          ),
+        );
+      }
+    }
+
+    camera.zoom = 4;
+    camera.snapTo(-size / 2);
+  }
+}
