398154658b11ae9f23b9f92d5076dc5c921f3e64
Ryuzaki <ufrshubham@gmail.com>
2022-08-20 12:26:55 +0530

    Player movement

--- lib/game/actors/player.dart
+++ lib/game/actors/player.dart
@@ -1,13 +1,23 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+import '../../utils/move_movement_detector.dart';
 
 enum PlayerState { idle, shoot, walk }
 
-class Player extends SpriteAnimationGroupComponent<PlayerState> {
+class Player extends PositionComponent
+    with KeyboardHandler, MouseMovementDetectorAwareComponent {
+  final SpriteAnimationGroupComponent<PlayerState> _animationGroupComponent;
+  Vector2 moveDirection = Vector2.zero();
+  double speed = 100;
+
   Player({
-    super.animations,
-    super.current,
-    super.removeOnFinish,
-    super.paint,
+    Map<PlayerState, SpriteAnimation>? animations,
+    PlayerState? current,
+    Map<PlayerState, bool>? removeOnFinish,
+    Paint? paint,
     super.position,
     super.size,
     super.scale,
@@ -15,5 +25,58 @@ class Player extends SpriteAnimationGroupComponent<PlayerState> {
     super.anchor,
     super.children,
     super.priority,
-  });
+  }) : _animationGroupComponent = SpriteAnimationGroupComponent<PlayerState>(
+          animations: animations,
+          current: current,
+          removeOnFinish: removeOnFinish,
+          paint: paint,
+          size: size,
+          position: Vector2(0, -size!.y / 4),
+        ) {
+    add(_animationGroupComponent);
+  }
+
+  @override
+  void update(double dt) {
+    if (moveDirection.x == 0 && moveDirection.y == 0) {
+      _animationGroupComponent.current = PlayerState.idle;
+    } else {
+      _animationGroupComponent.current = PlayerState.walk;
+    }
+    position += moveDirection * speed * dt;
+    super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    moveDirection.x = 0;
+    moveDirection.y = 0;
+
+    moveDirection.y += (keysPressed.contains(LogicalKeyboardKey.keyW) ||
+            keysPressed.contains(LogicalKeyboardKey.arrowUp))
+        ? -1
+        : 0;
+    moveDirection.y += (keysPressed.contains(LogicalKeyboardKey.keyS) ||
+            keysPressed.contains(LogicalKeyboardKey.arrowDown))
+        ? 1
+        : 0;
+    moveDirection.x += (keysPressed.contains(LogicalKeyboardKey.keyA) ||
+            keysPressed.contains(LogicalKeyboardKey.arrowLeft))
+        ? -1
+        : 0;
+    moveDirection.x += (keysPressed.contains(LogicalKeyboardKey.keyD) ||
+            keysPressed.contains(LogicalKeyboardKey.arrowRight))
+        ? 1
+        : 0;
+
+    moveDirection.normalize();
+
+    return true;
+  }
+
+  @override
+  bool onMouseMove(PointerHoverInfo info) {
+    angle = Vector2(0, -1).angleToSigned(info.eventPosition.game - position);
+    return true;
+  }
 }
--- lib/game/invert_me.dart
+++ lib/game/invert_me.dart
@@ -1,10 +1,21 @@
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:invert_me/game/level/level.dart';
 
-class InvertMe extends FlameGame {
+import '../utils/move_movement_detector.dart';
+
+class InvertMe extends FlameGame
+    with HasKeyboardHandlerComponents, MouseMovementDetector, TapDetector {
   @override
   Future<void> onLoad() async {
     camera.viewport = FixedResolutionViewport(Vector2(640, 360));
     add(Level());
+  }
+
+  @override
+  void onMouseMove(PointerHoverInfo info) {
+    propagateToChildren<MouseMovementDetectorAwareComponent>(
+      (p0) => p0.onMouseMove(info),
+    );
   }
 }
