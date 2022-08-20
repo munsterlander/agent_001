66ca49373f3a92fffd24491712f96ccc5d33fee5
Ryuzaki <ufrshubham@gmail.com>
2022-08-20 14:38:28 +0530

    Add wall block component

--- lib/game/level/level.dart
+++ lib/game/level/level.dart
@@ -1,5 +1,6 @@
 import 'package:flame/components.dart';
-import 'package:flutter/rendering.dart';
+import 'package:flame_mini_sprite/flame_mini_sprite.dart';
+import 'package:flutter/material.dart';
 import 'package:invert_me/assets/assets.dart';
 import 'package:invert_me/game/actors/player.dart';
 import 'package:invert_me/game/invert_me.dart';
@@ -7,20 +8,7 @@ import 'package:invert_me/utils/constants.dart';
 import 'package:invert_me/utils/minisprite_ext.dart';
 import 'package:mini_sprite/mini_sprite.dart';
 
-enum ObjectType { player, enemy }
-
-extension ObjectString on ObjectType {
-  String get name {
-    switch (this) {
-      case ObjectType.player:
-        return 'player_stand';
-      case ObjectType.enemy:
-        return 'enemy';
-      default:
-        return '';
-    }
-  }
-}
+import 'wall_block.dart';
 
 class Level extends Component with HasGameRef<InvertMe> {
   Level({Iterable<Component>? children, int? priority});
@@ -28,35 +16,257 @@ class Level extends Component with HasGameRef<InvertMe> {
   @override
   Future<void>? onLoad() async {
     final map = MiniMap.fromDataString(level1Map);
+    final blockSprites = await MiniLibrary.fromDataString(blocks).toSprites(
+      color: Colors.white,
+      pixelSize: 1,
+    );
 
     for (final objectData in map.objects.entries) {
       for (final data in objectData.value.entries) {
         switch (data.key) {
           case 'sprite':
-            if (data.value == ObjectType.player.name) {
-              final playerAnimationStateMap = {
-                PlayerState.idle:
-                    await gameRef.loadSpriteAnimationFromDataString(player),
-                PlayerState.shoot: await gameRef
-                    .loadSpriteAnimationFromDataString(playerShootAnimation),
-                PlayerState.walk: await gameRef
-                    .loadSpriteAnimationFromDataString(playerWalkAnimation),
-              };
+            {
+              switch (data.value) {
+                case SpriteIds.playerStand:
+                  {
+                    final playerAnimationStateMap = {
+                      PlayerState.idle: await gameRef
+                          .loadSpriteAnimationFromDataString(player),
+                      PlayerState.shoot:
+                          await gameRef.loadSpriteAnimationFromDataString(
+                              playerShootAnimation),
+                      PlayerState.walk:
+                          await gameRef.loadSpriteAnimationFromDataString(
+                              playerWalkAnimation),
+                    };
 
-              final playerComponent = Player(
-                animations: playerAnimationStateMap,
-                current: PlayerState.walk,
-                size: Vector2.all(32),
-                anchor: Anchor.center,
-                position: Vector2(
-                  objectData.key.x * gridSize,
-                  objectData.key.y * gridSize,
-                ),
-              );
-              add(playerComponent);
-              gameRef.camera.followComponent(playerComponent);
-            } else if (data.value == ObjectType.enemy.name) {}
-            break;
+                    final playerComponent = Player(
+                      animations: playerAnimationStateMap,
+                      current: PlayerState.walk,
+                      size: Vector2.all(gridSize),
+                      anchor: Anchor.center,
+                      position: Vector2(
+                        objectData.key.x * gridSize,
+                        objectData.key.y * gridSize,
+                      ),
+                    );
+                    add(playerComponent);
+                    gameRef.camera.followComponent(playerComponent);
+                    break;
+                  }
+                case SpriteIds.block:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.block]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockLeft:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockLeft]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockBottomLeft:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockBottomLeft]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockBottom:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockBottom]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockBottomRight:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockBottomRight]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockRight:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockRight]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockTopRight:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockTopRight]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockTop:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockTop]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockTopLeft:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockTopLeft]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockEndRight:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockEndRight]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockEndLeft:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockEndLeft]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockEndTop:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockEndTop]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockEndBottom:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockEndBottom]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockTopBottom:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockTopBottom]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+                case SpriteIds.blockLeftRight:
+                  {
+                    add(
+                      WallBlock(
+                        sprite: blockSprites[SpriteIds.blockLeftRight]!,
+                        position: Vector2(
+                          objectData.key.x * gridSize,
+                          objectData.key.y * gridSize,
+                        ),
+                        size: Vector2.all(gridSize),
+                      ),
+                    );
+                    break;
+                  }
+              }
+              break;
+            }
           default:
             debugPrint(data.toString());
         }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,20 @@
 const double gridSize = 22;
+
+class SpriteIds {
+  static const String playerStand = 'player_stand';
+  static const String block = 'block';
+  static const String blockLeft = 'block_left';
+  static const String blockBottomLeft = 'block_bottom_left';
+  static const String blockBottom = 'block_bottom';
+  static const String blockBottomRight = 'block_bottom_right';
+  static const String blockRight = 'block_right';
+  static const String blockTopRight = 'block_top_right';
+  static const String blockTop = 'block_top';
+  static const String blockTopLeft = 'block_top_left';
+  static const String blockEndRight = 'block_end_right';
+  static const String blockEndLeft = 'block_end_left';
+  static const String blockEndTop = 'block_end_top';
+  static const String blockEndBottom = 'block_end_bottom';
+  static const String blockTopBottom = 'block_top_bottom';
+  static const String blockLeftRight = 'block_left_right';
+}
