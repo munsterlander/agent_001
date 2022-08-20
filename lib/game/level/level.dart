02749de2216faee0718d1f6c5a65266ff3d89411
Ryuzaki <ufrshubham@gmail.com>
2022-08-20 11:26:28 +0530

    Spawn player from map file

--- lib/game/level/level.dart
+++ lib/game/level/level.dart
@@ -1,28 +1,65 @@
 import 'package:flame/components.dart';
+import 'package:flutter/rendering.dart';
 import 'package:invert_me/assets/assets.dart';
 import 'package:invert_me/game/actors/player.dart';
 import 'package:invert_me/game/invert_me.dart';
+import 'package:invert_me/utils/constants.dart';
 import 'package:invert_me/utils/minisprite_ext.dart';
+import 'package:mini_sprite/mini_sprite.dart';
+
+enum ObjectType { player, enemy }
+
+extension ObjectString on ObjectType {
+  String get name {
+    switch (this) {
+      case ObjectType.player:
+        return 'player';
+      case ObjectType.enemy:
+        return 'enemy';
+      default:
+        return '';
+    }
+  }
+}
 
 class Level extends Component with HasGameRef<InvertMe> {
   Level({Iterable<Component>? children, int? priority});
 
   @override
   Future<void>? onLoad() async {
-    final playerAnimationStateMap = {
-      PlayerState.idle: await gameRef.loadSpriteAnimationFromDataString(player),
-      PlayerState.shooting:
-          await gameRef.loadSpriteAnimationFromDataString(shootAnimation),
-    };
+    final map = MiniMap.fromDataString(level1Map);
 
-    add(
-      Player(
-        animations: playerAnimationStateMap,
-        current: PlayerState.shooting,
-        size: Vector2.all(32),
-        position: gameRef.size / 2,
-      ),
-    );
+    for (final objectData in map.objects.entries) {
+      for (final data in objectData.value.entries) {
+        switch (data.key) {
+          case 'sprite':
+            if (data.value == ObjectType.player.name) {
+              final playerAnimationStateMap = {
+                PlayerState.idle:
+                    await gameRef.loadSpriteAnimationFromDataString(player),
+                PlayerState.shooting: await gameRef
+                    .loadSpriteAnimationFromDataString(shootAnimation),
+              };
+
+              add(
+                Player(
+                  animations: playerAnimationStateMap,
+                  current: PlayerState.shooting,
+                  size: Vector2.all(32),
+                  position: Vector2(
+                    objectData.key.x * gridSize,
+                    objectData.key.y * gridSize,
+                  ),
+                ),
+              );
+            } else if (data.value == ObjectType.enemy.name) {}
+            break;
+          default:
+            debugPrint(data.toString());
+        }
+      }
+    }
+
     return super.onLoad();
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,1 @@
+const double gridSize = 22;
