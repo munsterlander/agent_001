d479b948a462473b73401045916f93d8ded93a58
Munsterlander <munsterlander@users.noreply.github.com>
2022-08-19 18:08:22 -0700

    Animations working!

--- lib/main.dart
+++ lib/main.dart
@@ -15,39 +15,48 @@ void main() {
 class InvertMe extends FlameGame {
   @override
   Future<void> onLoad() async {
-    final miniLibrary = MiniLibrary.fromDataString(library);
+    final miniLibrary = MiniLibrary.fromDataString(characterAnimation);
     final sprites = await miniLibrary.toSprites(
       color: Colors.white,
       pixelSize: 1,
     );
 
-    final miniMap = MiniMap.fromDataString(map);
+    final miniMap = MiniMap.fromDataString(animationMap);
     for (final entry in miniMap.objects.entries) {
       final sprite = sprites[entry.value['sprite']];
       if (sprite != null) {
-        print(entry.value['sprite']);
-        unawaited(
-          add(
-            SpriteComponent(
-              sprite: sprite,
-              position: Vector2(
-                (entry.key.x * 8).toDouble(),
-                (entry.key.y * 8).toDouble(),
+        final information = entry.value['sprite'].split('_');
+        switch (information[0].toString()) {
+          case 'character':
+            final int animationLength = int.parse(information[1]);
+            List<Sprite> spriteList = [];
+            for (int i = 1; i <= animationLength; i++) {
+              final spriteName = '${information[0]}_${information[1]}_$i';
+              spriteList.add(sprites[spriteName]);
+            }
+
+            final animation = SpriteAnimation.spriteList(spriteList,
+                stepTime: .2, loop: true);
+
+            unawaited(add(
+              SpriteAnimationComponent(
+                animation: animation,
+                position: Vector2(
+                  (entry.key.x * 8).toDouble(),
+                  (entry.key.y * 8).toDouble(),
+                ),
+                size: Vector2(
+                  sprite.image.width.toDouble(),
+                  sprite.image.height.toDouble(),
+                ),
               ),
-              size: Vector2(
-                sprite.image.width.toDouble(),
-                sprite.image.height.toDouble(),
-              ),
-            ),
-          ),
-        );
+            ));
+            break;
+        }
       }
     }
 
     camera.zoom = 4;
     camera.snapTo(-size / 2);
   }
-
-  @override
-  Future<void> update(double dt) async {}
 }
