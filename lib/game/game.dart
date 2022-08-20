44392c0cb19794420ee331df085c0a9a5328c221
Ryuzaki <ufrshubham@gmail.com>
2022-08-20 15:03:06 +0530

    Change package/game name

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,14 +1,14 @@
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:invert_me/game/level/level.dart';
 
 import '../utils/move_movement_detector.dart';
+import 'level/level.dart';
 
-class InvertMe extends FlameGame
+class Agent001 extends FlameGame
     with HasKeyboardHandlerComponents, MouseMovementDetector, TapDetector {
   @override
   Future<void> onLoad() async {
-    camera.viewport = FixedResolutionViewport(Vector2(640, 360));
+    camera.viewport = FixedResolutionViewport(Vector2(640, 360)/2);
     add(Level());
   }
 
