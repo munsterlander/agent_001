d922c1fc947c9425d2122ecebfa67b3d1623ea57
Munsterlander <munsterlander@users.noreply.github.com>
2022-08-21 08:38:22 -0700

    Added game won

---
+++ lib/game/overlays/game_won.dart
@@ -0,0 +1,78 @@
+import 'package:agent_001/game/game.dart';
+import 'package:agent_001/utils/constants.dart';
+import 'package:pixel_border/pixel_border.dart';
+import 'package:agent_001/game/level/level.dart';
+import 'package:flutter/material.dart';
+
+class GameWon extends StatelessWidget {
+  // An unique identified for this overlay.
+  static const id = 'GameWon';
+
+  // Reference to parent game.
+  final Agent001Game gameRef;
+
+  const GameWon({super.key, required this.gameRef});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          decoration: ShapeDecoration(
+            shape: PixelBorder.solid(
+              pixelSize: 2.0,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(8.0),
+                bottomRight: Radius.circular(16.0),
+              ),
+              color: whiteTextColor,
+            ),
+          ),
+          height: 200,
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Congratulations!\n\n001 in Style!',
+                style: TextStyle(
+                  color: whiteTextColor,
+                  fontSize: 24,
+                ),
+              ),
+              const SizedBox(height: 40),
+              SizedBox(
+                width: 200,
+                height: 75,
+                child: ElevatedButton(
+                  onPressed: () {
+                    gameRef.overlays.remove(id);
+                    gameRef.add(
+                      Level(levelData: gameRef.getLevelData(0)!),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: whiteTextColor,
+                    shape: PixelBorder.shape(
+                      borderRadius: BorderRadius.circular(10.0),
+                      pixelSize: 5.0,
+                    ),
+                  ),
+                  child: const Text(
+                    'Play Again',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: blackTextColor,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:agent_001/game/overlays/instructions.dart';
 import 'package:agent_001/game/overlays/main_menu.dart';
 import 'package:agent_001/game/overlays/credits.dart';
 import 'package:agent_001/game/overlays/game_over.dart';
+import 'package:agent_001/game/overlays/game_won.dart';
 import 'package:agent_001/game/overlays/settings_menu.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:pixel_border/pixel_border.dart';
 
 import 'game/game.dart';
 
@@ -43,6 +43,8 @@ class Agent001GameApp extends StatelessWidget {
             Instructions.id: (_, gameRef) => Instructions(gameRef: gameRef),
             GameOver.id: (_, Agent001Game gameRef) =>
                 GameOver(gameRef: gameRef),
+            GameWon.id: (_, Agent001Game gameRef) =>
+                GameWon(gameRef: gameRef),
             SettingsMenu.id: (_, Agent001Game gameRef) =>
                 SettingsMenu(gameRef: gameRef),
             Credits.id: (_, Agent001Game gameRef) => Credits(gameRef: gameRef),
