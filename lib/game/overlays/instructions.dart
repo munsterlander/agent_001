2317319f1407a81a3a32b1e6c4f0a1415d4ac772
Munsterlander <munsterlander@users.noreply.github.com>
2022-08-21 00:13:52 -0700

    Fixed file errors

--- lib/game/overlays/instructions.dart
+++ lib/game/overlays/instructions.dart
@@ -1,48 +1,77 @@
 import 'package:agent_001/game/game.dart';
 import 'package:agent_001/utils/constants.dart';
-import 'package:flame/components.dart';
-import 'package:flame/input.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pixel_border/pixel_border.dart';
+import 'package:agent_001/game/level/level.dart';
+import 'package:flutter/material.dart';
 
-class Instructions extends PositionComponent with HasGameRef<Agent001Game> {
-  Instructions({
-    super.position,
-    super.size,
-    super.scale,
-    super.angle,
-    super.anchor,
-    super.children,
-    super.priority,
-  }) {
-    positionType = PositionType.viewport;
-  }
+class Instructions extends StatelessWidget {
+  // An unique identified for this overlay.
+  static const id = 'Instructions';
 
-  late ButtonComponent closeButton;
+  // Reference to parent game.
+  final Agent001Game gameRef;
+
+  const Instructions({super.key, required this.gameRef});
 
   @override
-  Future<void>? onLoad() async {
-    add(
-      closeButton = ButtonComponent(
-        button: TextComponent(
-          textRenderer: pixelFont14,
-          text: 'These are instructions and click to close',
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              decoration: ShapeDecoration(
+                shape: PixelBorder.solid(
+                  pixelSize: 2.0,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(8.0),
+                    bottomRight: Radius.circular(16.0),
+                  ),
+                  color: Colors.purple,
+                ),
+              ),
+              height: 200,
+              width: 400,
+            ),
+            SizedBox(
+              width: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                  gameRef.overlays.remove(id);
+                  gameRef.add(Level(levelData: gameRef.getLevelData(0)!));
+                },
+                child: Text(
+                  'Play',
+                  style: GoogleFonts.pressStart2p(
+                    fontSize: 14.0,
+                    color: whiteTextColor,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                  gameRef.overlays.remove(id);
+                  //gameRef.overlays.add(Settings.id);
+                },
+                child: Text(
+                  'Settings',
+                  style: GoogleFonts.pressStart2p(
+                    fontSize: 14.0,
+                    color: whiteTextColor,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
-        position: gameRef.size / 2,
-        anchor: Anchor.center,
-        onReleased: () {
-          gameRef.router.pop();
-        },
       ),
     );
-
-    add(
-      NineTileBoxComponent(
-        nineTileBox: NineTileBox(gameRef.getSprite(SpriteIds.dialog)!),
-        position: closeButton.position,
-        size: closeButton.size,
-        anchor: Anchor.center,
-      ),
-    );
-
-    return super.onLoad();
   }
 }
